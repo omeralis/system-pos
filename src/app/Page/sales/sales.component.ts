4db9910fad114c7093c01c08ea2df04ffd9ea3c0
omeralis <omeralis2000@gmial.com>
2021-10-28 17:41:21 +0200

    [add] add post add order

--- src/app/Page/sales/sales.component.ts
+++ src/app/Page/sales/sales.component.ts
@@ -12,69 +12,37 @@ import { orderLine } from 'src/app/shared/model/order/orderLine';
 })
 export class SalesComponent implements OnInit {
   ItemOfgroupData: any = [];
+  PriceItem: any;
   // Items:Groups[]=[];
   active = 0;
   cartlist: any = [];
-  lastOrderID:any=[];
-  dataOrder:orderLine[]=[];
-  AddForm: FormGroup | any;
+  lastOrderID: any = [];
+  dataOrder: orderLine[] = [];
+  // AddForm: FormGroup | any;
   Qut = 1;
-  
-	data=[
-        {
-          "orderId": 1,
-          "itemNo": 1,
-          "quantity": 1,
-          "unitPrice": 1,
-          "subTotal": 1
-        },
-          {
-          "orderId": 1,
-          "itemNo": 1,
-          "quantity": 1,
-          "unitPrice": 1,
-          "subTotal": 1
-        }
-]
+// a:any;
+  // orderNo: any;
 
-  constructor(
-    private modalService: NgbModal,
-    public Service: ServicesService,
-    private formBuilder: FormBuilder,
-  ) { }
+  constructor(private modalService: NgbModal, public Service: ServicesService, private formBuilder: FormBuilder,) { }
 
   orderForm = new FormGroup({
     id: new FormControl(),
-    customerNo: new FormControl(1,[Validators.required]),
-    saleDate: new FormControl('2021-10-14',[Validators.required]),
-    storeNo: new FormControl(1,[Validators.required])
+    customerNo: new FormControl(1, [Validators.required]),
+    saleDate: new FormControl('2021-10-14', [Validators.required]),
+    storeNo: new FormControl(1, [Validators.required])
   });
-  orderLineForm = new FormGroup({
-    id: new FormControl(null),
-    orderId: new FormControl(this.orderForm.value.id,[Validators.required]),
-    itemNo: new FormControl(null,[Validators.required]),
-    quantity: new FormControl(null,[Validators.required]),
-    unitPrice: new FormControl(null,[Validators.required]),
-    subTotal: new FormControl(null,[Validators.required]),
-  })
-  private AddOrderForm() {
-    this.AddForm = this.formBuilder.group({
-      data:this.formBuilder.array([
-        this.formBuilder.group({
-          orderId: [1],
-          itemNo:[null],
-          quantity:[10],
-          unitPrice:[null],
-          subTotal:[null],
-        }),
-      ])
-    });
-  }
+  // orderLineForm = new FormGroup({
+  //   id: new FormControl(null),
+  //   orderId: new FormControl(this.orderForm.value.id,[Validators.required]),
+  //   itemNo: new FormControl(null,[Validators.required]),
+  //   quantity: new FormControl(null,[Validators.required]),
+  //   unitPrice: new FormControl(null,[Validators.required]),
+  //   subTotal: new FormControl(null,[Validators.required]),
+  // })
+
 
   ngOnInit(): void {
     this.getItemOfGroup();
-    this.AddOrderForm();
-    this.AddForm.get('data') as FormArray;
   }
 
   getItemOfGroup() {
@@ -82,24 +50,28 @@ export class SalesComponent implements OnInit {
       this.ItemOfgroupData = GroupRespone;
     });
   }
+
   addToCart(cartToAdd: any) {
     if (this.cartlist.length == 0) {
-      this.cartlist.push(cartToAdd);
-      console.log('1:');
-    } else {
+      cartToAdd.quantity = 1;
+     
+      cartToAdd.unitPrice = cartToAdd.store_item[0].priceItem;
+      cartToAdd.subTotal = cartToAdd.quantity * cartToAdd.unitPrice;
 
-      for (let i=0; i <  this.cartlist.length; i++) {
-        console.log("if tow :", i +"  ling",this.cartlist.length)
-        if ( this.cartlist[i] == cartToAdd) 
-        {
-          this.Qut++;
-          console.log('2:',  this.cartlist[i].itemName, cartToAdd.itemName);
+      this.cartlist.push(cartToAdd);
+    } else {
+      for (let i = 0; i < this.cartlist.length; i++) {
+        if (this.cartlist[i] == cartToAdd) {
+          this.cartlist[i].quantity += 1;
+          cartToAdd.subTotal = cartToAdd.quantity * cartToAdd.unitPrice;
           break;
         }
-         else if( this.cartlist[i] != cartToAdd && i+1 ==this.cartlist.length)
-         {
+        else if (this.cartlist[i] != cartToAdd && i + 1 == this.cartlist.length) {
+          cartToAdd.quantity = 1;
+
+          cartToAdd.unitPrice = cartToAdd.store_item[0].priceItem;
+          cartToAdd.subTotal = cartToAdd.quantity * cartToAdd.unitPrice;
           this.cartlist.push(cartToAdd);
-          console.log('3:',  this.cartlist[i].itemName, cartToAdd.itemName);
           break;
         }
 
@@ -107,38 +79,35 @@ export class SalesComponent implements OnInit {
     }
   }
 
-  AddIOrder(){
+  AddIOrder() {
     this.Service.postSales(this.orderForm.value).subscribe(
-      (OrderRespone)=>{
+      (OrderRespone) => {
         this.modalService.dismissAll();
         this.lastOrderID = OrderRespone;
-        this.AddForm.value.orderId = this.lastOrderID.id;
-        console.log( this.AddForm.value.orderId)
-        this.Service.postSalesLine(this.AddForm.value).subscribe(
-          (OrderLineRespone)=>{
-            this.modalService.dismissAll();
-            console.log("OrderLineRespone" , OrderLineRespone);
+        console.log('this.cartlist 1 :', this.cartlist);
+        this.cartlist.orderId = this.lastOrderID.id,
+        console.log(this.cartlist.orderId);
+        this.Service.postSalesLine(this.cartlist).subscribe(
+          (OrderLineRespone) => {
+            console.log("OrderLineRespone", OrderLineRespone);
           },
           (error) => {
             console.log(error);
           },
-          ()=>{
+          () => {
           }
         )
-        // }
       },
       (error) => {
         console.log(error);
       },
-      ()=>{
-       // alert("seccess")
+      () => {
+        // alert("seccess")
       }
     );
   }
 
-  get dataControls() {
-    return this.AddForm.get('data') as FormArray;
- }
+
   removeToCart(cartITem: any) {
     this.cartlist.pop(cartITem);
   }
--- src/app/shared/model/order/orderLine.ts
+++ src/app/shared/model/order/orderLine.ts
@@ -1,11 +1,11 @@
 import { order } from "./order";
 
 export interface orderLine{
-    id?: number,
-    orderId?: number,
-    itemNo?:string,
-    quantity?:number,
-    unitPrice :number,
-    subTotal :number,
-    order:order;
+      id?: number,
+      orderId?: number,
+      itemNo?:string,
+      quantity?:number,
+      unitPrice :number,
+      subTotal :number,
+      order:order;
   }
